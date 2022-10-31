5695e02a5bc5fd37e8408db27ea238d7cb41dad4
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-10-31 14:33:43 +0800

    Update auditonly.sh

--- auditonly.sh
+++ auditonly.sh
@@ -67,7 +67,7 @@ UID_MIN=$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)
 -a always,exit -F arch=b32 -S chmod,fchmod,fchmodat -F auid>=${UID_MIN} -F auid!=unset -F key=perm_mod
 -a always,exit -F arch=b32 -S lchown,fchown,chown,fchownat -F auid>=${UID_MIN} -F auid!=unset -F key=perm_mod
 -a always,exit -F arch=b32 -S setxattr,lsetxattr,fsetxattr,removexattr,lremovexattr,fremovexattr -F auid>=${UID_MIN} -F auid!=unset -F key=perm_mod
-" >> /etc/audit/rules.d/50-perm_mod.rules || printf "ERROR: Variable'UID_MIN' is unset.\n"
+" >> /etc/audit/rules.d/50-perm_mod.rules
 }
 
 augenrules --load
